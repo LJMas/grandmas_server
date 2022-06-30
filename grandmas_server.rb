63a14beadbb45374eaddff6fd05a5c505309b54d
LJMas <lucy.massie@madetech.com>
2022-06-30 16:33:32 +0100

    Grandma's server has two pages. One welcomes you to the server, by name if you enter one, and allows you to click a button taking you to the second page. The second page has pictures of dogs and a button that will take you back to the welcome page, with an option to enter your name to be greeted by name.

--- grandmas_server.rb
+++ grandmas_server.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
+get '/dogs' do
+    erb :dogs
+end
+
+post '/dogs' do
+    redirect '/' + params[:name] if params[:name]
+    redirect '/'
+end
+
 get '/:name?' do | name |
-    name = "Grandma" if !name
-    "Hello #{name}!"
-  end
+    name = name + ", " if name
+    erb :index, :locals => {:name=>name} 
+end
--- spec/grandmas_server_spec.rb
+++ spec/grandmas_server_spec.rb
@@ -11,5 +11,36 @@ RSpec.describe "Grandma's server" do
     def app
       Sinatra::Application
     end
+    context "Root" do
+        it "should give a greeting if no parameter is given" do
+            get '/'
+            expect(last_response).to be_ok
+            expect(last_response.body).to include "Hi, welcome to Grandma's server!"
+        end
 
+        it "should greet by name if a name parameter is given" do
+            get '/Lucy'
+            # puts last_response.inspect
+            expect(last_response).to be_ok
+            expect(last_response.body).to include "Hi, Lucy, welcome to Grandma's server!"
+        end
+    end
+    context "Dogs" do
+        it "should have a second page called dogs" do
+            get '/dogs'
+            expect(last_response).to be_ok
+        end
+        it "should have a heading that says 'Grandma likes dogs!'" do
+            get '/dogs'
+            expect(last_response.body).to include "<h1>Grandma likes dogs!</h1>"
+        end
+        it "should have at least one image" do
+            get '/dogs'
+            expect(last_response.body).to include "<img"
+        end
+        it "should be linked to a style sheet" do
+            get '/dogs'
+            expect(last_response.body).to include "styles.css"
+        end
+    end
 end
